61eb8decb17e6891af49d10d25f66cfd1f356a3e
root <gabriel1235897@gmail.com>
2021-10-11 17:32:08 -0300

    Tela principal

---
+++ lpw/resources/views/tamplate/tamplate.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+@yield('conteudo')
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Estudante-IFPE</title>
+    <style>
+        
+        .titulo{
+            margin:2% 2% 0 ;
+            background-color: blue;
+            border-radius: 150px;
+        }
+        #nome-titulo{
+            color:white;
+            text-align: center;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="titulo">
+        <h1 id="nome-titulo">
+            Onde você aprende Matemática 
+        </h1>
+    </div>
+    <nav class="navbar navbar-light bg-light">
+        <div class="navbar navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            <img src="" alt="">
+          </a>
+          <a class="navbar-brand" href="#">
+            1°  Logaritmo e Funçõe Logarítmica 
+          </a>
+
+          <a class="navbar-brand" href="#">
+            2° Trigonometria na circunferência e Números complexos
+        </a>
+        <br>
+        <a class="navbar-brand" href="#">
+            3° Análise Combinatória e Probabilidade 
+        </a>
+        <br>
+        <a class="navbar-brand" href="#">
+            4° Geometria analitica(Parte de Cônicas), Limites e Derivadas.
+        </a>
+        </div>
+      </nav>
+</body>
+</html>
