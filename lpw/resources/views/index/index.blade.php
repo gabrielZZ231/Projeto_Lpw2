f5789b8921369f79c713a0d185b3e45f4b0b5dcd
João Victor Guimarães de Araújo <fulanodetal@exemplo.br>
2021-12-05 21:51:09 -0300

    boa noite

--- lpw/resources/views/index/index.blade.php
+++ lpw/resources/views/index/index.blade.php
@@ -1,15 +1,71 @@
 @extends('tamplate.tamplate')
 @section('conteudo')
 <div class="index">
-  <!-- Imagem e textninho da parte de cima!-->
-        <div class = "foco">
-            <h2 id = "textinho">Resumos e exercícios de <br>Matemática</h2>
-        </div>
-    <form>
-        <div class="form-group">
-        <label>Aula X</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </form>
+  <!-- Banner!-->
+        <section class = "classe">
+                <div class = "foco">
+                    <h2 id = "textinho">Resumos e exercícios de <br>Matemática</h2>
+                </div>
+        </section>
+
+    <!-- Serviços!-->  
+    <section class = "srvs">
+                <div class = "focoServicos">
+                    <h2 >Recursos</h2>
+                    <ul>
+                        <li class = "serv-img grid-1-3"> <img src="assets/livrosEmpilhados.png" alt="img assuntos">
+                                                         <h3>Conteúdo</h3>   
+                                                         <p>Recomendados, mas não <br>de nossa autoria.</p>
+                        </li>
+                        <li class = "serv-img grid-1-3"> <img src="assets/exerciciosMat.png" alt="img exercicios">
+                                                         <h3>exercícios</h3>
+                                                         <p>Recomendados, mas não <br>de nossa autoria.</p>
+                        </li>
+                    </ul>
+                </div>
+        </section>  
+
+        <!-- Assuntos!-->  
+    <section class = "ass">
+                <div class = "focoAssuntosP">
+                    <h2 >Conteúdos</h2>
+                    <ul>
+                        <li class = "serv-img grid-1-3"> <img src="assets/log.png" alt="img logaritmos">
+                                                         <h3>Logarítmo</h3>   
+                                                         <div class = "outer">
+                                                            <a href="#" class="inner">ACESSE</a>
+                                                         </div>
+                        </li>
+                        <li class = "serv-img grid-1-3"> <img src="assets/NumerosComplexos.png" alt="img n complexos">
+                                                         <h3>Númeoros <br>Complexos</h3>
+                                                         <div class = "outer">
+                                                         <a href="#" class="inner">ACESSE</a>
+                                                         </div>
+                        </li>
+                    </ul>
+                </div>
+        </section>
+
+        <section class = "assS">
+                <div class = "focoAssuntosS">
+                    <ul>
+                        <li class = "serv-img grid-1-3"> <img src="assets/geoAna.png" alt="img logaritmos">
+                                                         <h3>Geometria <br>Analítica
+                                                         </h3>   
+                                                         <div class = "outerS">
+                                                            <a href="#" class="ner">ACESSE</a>
+                                                         </div>
+                        </li>
+                        <li class = "serv-img grid-1-3"> <img src="assets/trigonometria.png" alt="img trigonometria">
+                                                         <h3>Trigonometria</h3>
+                                                         <div class = "outerS">
+                                                         <a href="#" class="ner">ACESSE</a>
+                                                         </div>
+                        </li>
+                    </ul>
+                </div>
+        </section>
+
         <footer class="bg-dark text-light fixed-bottom">
         <div class="text-center" style="background-color: #333; padding: 1px 20px;" >
         <h3>Sobre</h3>
